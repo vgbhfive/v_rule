47c962d144bf851e475c003f092f8721fa219f65
vgbhfive <vgbhfive@foxmail.com>
2025-11-30 21:14:33 +0800

    deploy

--- src/main/java/com/vgbhfive/v_rule/common/exception/DataBaseException.java
+++ src/main/java/com/vgbhfive/v_rule/common/exception/DataBaseException.java
@@ -5,7 +5,7 @@ package com.vgbhfive.v_rule.common.exception;
  * @author: vgbhfive
  * @date: 2025/11/25 23:38
  */
-public class DataBaseException extends RuntimeException{
+public class DataBaseException extends RuntimeException {
 
     public DataBaseException(String message) {
         super(message);
